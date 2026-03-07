e45254485880ae66cd8378d2195bc86fc7abd577
Rahul Barnwal <78689930+rahulXbarnwal@users.noreply.github.com>
2026-03-07 18:49:44 +0530

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 
 // In a file, there can be multiple classes but there will be just one public class
+// multiple classes in the same Java file can have a main method
+// Java does NOT automatically start from the main method of the public class.
+// It starts from the class you explicitly tell the JVM to run
 
 public class Hello { // this is a block
 
