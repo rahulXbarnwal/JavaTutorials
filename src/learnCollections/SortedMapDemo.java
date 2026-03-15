04211cad4966976e6b6620158c67dbf1f4e9fcce
Rahul Barnwal <78689930+rahulXbarnwal@users.noreply.github.com>
2026-03-15 16:10:52 +0530

    Create SortedMapDemo.java

---
+++ src/learnCollections/SortedMapDemo.java
@@ -0,0 +1,32 @@
+package learnCollections;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/*
+SortedMap is an Interface that extends Map and guarantees that the entries are sorted based on the keys, either in their natural ordering or by a specified Comparator. 
+ */
+
+public class SortedMapDemo {
+    public static void main(String[] args) {
+        // Key (Integer) implements Comparable, so it will be sorted in natural order (numerical)
+        // else we can pass custom comparator to the TreeMap constructor
+        SortedMap<Integer, String> map = new TreeMap<>();
+        map.put(91, "Vivek");
+        map.put(99, "Shubham");
+        map.put(78, "Mohit");
+        map.put(77, "Vipul");
+        // map.put(null, "Rahul"); // null keys are not allowed in TreeMap, it will throw NullPointerException
+
+        System.out.println(map); // {77=Vipul, 78=Mohit, 91=Vivek, 99=Shubham}
+
+        System.out.println(map.firstKey());    // 77
+        System.out.println(map.lastKey());     // 99
+        System.out.println(map.headMap(91));   // {77=Vipul, 78=Mohit} -> exclude 91
+        System.out.println(map.tailMap(91));   // {91=Vivek, 99=Shubham} -> include 91
+
+
+        // For keys in descending order
+        SortedMap<Integer, String> map1 = new TreeMap<>((a, b) -> b - a);
+    }
+}
